3683ac41415a6819f97c0b37220cd2407dae0c5d
oppen321 <zj18139624826@gmail.com>
2024-10-12 19:27:46 +0800

    Update diy.office.sh

--- diy.office.sh
+++ diy.office.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+# 个性化设置
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/MI-R3G/' package/base-files/files/bin/config_generate
+
+# 调整菜单位置
+sed -i "s|services|system|g" feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
+
+# turboacc 补丁
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+
+# 安装插件
+./scripts/feeds update -l
+./scripts/feeds install -a
