604e176614152f1158eafd5c0603db685fb417ef
oppen321 <zj18139624826@gmail.com>
2024-10-14 19:37:28 +0800

    Update Cetron-CT3003.sh

--- Diy/Cetron-CT3003.sh
+++ Diy/Cetron-CT3003.sh
@@ -3,7 +3,7 @@
 # DaoDao's script 
 #=================================================
 ##添加自己的插件库
-echo -e "\nsrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk" >> feeds.conf.default
+echo -e "\nsrc-git Zero-ipk https://github.com/oppen321/Zero-ipk" >> feeds.conf.default
 
       
 ##
