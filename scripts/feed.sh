2ef21f48cfcfa6f7b6560cd535cb0e1da1214f24
oppen321 <zj18139624826@gmail.com>
2024-10-15 02:24:31 +0800

    Update feed.sh

--- scripts/feed.sh
+++ scripts/feed.sh
@@ -3,4 +3,4 @@
 # oppen321's script 
 #=================================================
 ##添加自己的插件库
-echo -e "\nsrc-git Zero-IPK https://github.com/oppen321/Zero-ipk" >> feeds.conf.default
+echo -e "\nsrc-git Zero https://github.com/oppen321/Zero-ipk" >> feeds.conf.default
