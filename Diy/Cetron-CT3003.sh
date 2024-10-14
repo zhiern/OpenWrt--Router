9f28028350908261aa397012bd3a5ada392a19f5
oppen321 <zj18139624826@gmail.com>
2024-10-14 19:50:22 +0800

    Update Cetron-CT3003.sh

--- Diy/Cetron-CT3003.sh
+++ Diy/Cetron-CT3003.sh
@@ -2,8 +2,21 @@
 #=================================================
 # DaoDao's script 
 #=================================================
-##添加自己的插件库
-echo -e "\nsrc-git Zero-ipk https://github.com/oppen321/Zero-ipk" >> feeds.conf.default
+
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+
+# Zero 插件
+git clone --depth=1 https://github.com/oppen321/Zero-ipk package/Zero-ipk
 
       
 ##
