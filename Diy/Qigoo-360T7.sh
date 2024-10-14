8ff19df54a2f528bcc37e3ff9dbea1601ff086ee
oppen321 <zj18139624826@gmail.com>
2024-10-14 08:25:16 +0800

    Update Qigoo-360T7.sh

--- Diy/Qigoo-360T7.sh
+++ Diy/Qigoo-360T7.sh
@@ -38,6 +38,9 @@ sed -i 's/set wireless.default_${name}.ssid=OpenWrt/set wireless.default_${name}
 # 开启Wifi
 sed -i 's/set wireless.${name}.disabled=1/set wireless.${name}.disabled=0/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
+# WiFi设置
+cp -f $GITHUB_WORKSPACE/scripts/mac80211.sh package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
 # 调整菜单位置
 sed -i "s|services|system|g" feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
 
