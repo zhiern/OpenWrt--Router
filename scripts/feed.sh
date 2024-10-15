8db8ed0659c12696426bd3369a4592ba0426d644
oppen321 <zj18139624826@gmail.com>
2024-10-15 20:37:44 +0800

    Delete scripts/feed.sh

--- scripts/feed.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#=================================================
-# oppen321's script 
-#=================================================
-##添加自己的插件库
-echo 'src-git Zero https://github.com/oppen321/Zero-ipk' >>feeds.conf.default
